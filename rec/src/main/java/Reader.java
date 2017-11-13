a4abc250896082ec1b2c22efe4746139823d0364
João Victor <jvas@ic.ufal.br>
2017-11-13 00:09:43 -0300

    useless commit

--- rec/src/main/java/Reader.java
+++ rec/src/main/java/Reader.java
@@ -1,20 +1,35 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 import com.opencsv.CSVReader;
 
 public class Reader {
 
-	public static void whatever(){
-		String csvFile = "C:\\Users\\joaov\\workspace\\rec\\bin\\ml-100k\\test.csv";
-
-		CSVReader reader = null;
+	public static void readFile(User user) {
+		String csvFile = "C:/Users/Victor/Documents/GitHub/Movie-Recommender-System/rec/src/main/resources/ml-100k";
+		
 		try {
-			reader = new CSVReader(new FileReader(csvFile));
-			String[] line;
-			while ((line = reader.readNext()) != null) {
-				System.out.println("Country [id= " + line[0] + ", code= " + line[1] + " , name=" + line[2] + "]");
+			FileReader in = new FileReader(csvFile);
+			Iterable<CSVRecord> records = CSVFormat.TDF.withHeader("uID", "Movie", "Rating", "TimeStamp").parse(in);
+			for (CSVRecord record : records) {
+				int index = Integer.parseInt(record.get("uID"));
+				if(index == user.id) {
+					
+					int aux = Integer.parseInt(record.get("Movie"));
+					int aux2 = Integer.parseInt(record.get("Rating"));
+					user.ratings[aux] = aux2;
+				}
 			}
+			user.showData();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
